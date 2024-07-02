8306d5b58ff5c46d927e4438468eee02e2c4763b
luisgcostaguedes <luisgcostaguedes@gmail.com>
2024-07-02 17:30:13 -0300

    ajust

---
+++ JavaAulas.java
@@ -0,0 +1,7 @@
+public class JavaAulas {
+  public static void main(String[] args) {
+    double salarioAnual = 98275.67;
+    int valor = (int) salarioAnual;
+    System.out.println(valor);
+  }
+}
