ae32d6ef42bfd112a0c598b7157d0c5009e1ee06
luisgcostaguedes <luisgcostaguedes@gmail.com>
2024-07-03 20:30:26 -0300

    ajust

--- EstruturaCondicional.java
+++ EstruturaCondicional.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class EstruturaCondicional {
+public class EstruturaCondicional {
+    public static void main(String[] args) {
+        double media = 7.5;
+        if (media >= 7.0) {
+            System.out.println("Aprovado");
+        } else if (media >= 5.0) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+
 }
