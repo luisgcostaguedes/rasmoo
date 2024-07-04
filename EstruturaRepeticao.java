5e2e010d698cf75f830372032999fb68331a7a8e
luisgcostaguedes <luisgcostaguedes@gmail.com>
2024-07-04 08:57:34 -0300

    ajust IDE

--- EstruturaRepeticao.java
+++ EstruturaRepeticao.java
@@ -1,10 +1,10 @@
 public class EstruturaRepeticao {
   public static void main(String[] args) {
     int contador = 4;
-//    while (contador < 10) {
-//      System.out.println("Contador while: " + contador);
-//      contador++;
-//    }
+    while (contador < 10) {
+      System.out.println("Contador while: " + contador);
+      contador++;
+    }
     System.out.println("----------------------------------------------------");
 
     for (int i = 0; i < 5; i++) {
@@ -13,9 +13,9 @@ public class EstruturaRepeticao {
 
     System.out.println("----------------------------------------------------");
 
-    do {
-      System.out.println("Contador do while: " + contador);
-      contador++;
-    } while (contador < 10);
+    // do {
+    // System.out.println("Contador do while: " + contador);
+    // contador++;
+    // } while (contador < 10);
   }
 }
--- out/production/rasmoo_class/README.md
+++
@@ -1,3 +0,0 @@
-# Curso completo de JAVA da plataforma rasmoo!
-
-Tudo que irei desenvolver durante o curso, será documentado aqui neste repositório
